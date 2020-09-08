4a0531a0ea71200d47043995076cbbfdda5c7001
yuwentao114 <49578217+yuwentao114@users.noreply.github.com>
2020-09-09 07:56:31 +0800

    Create add_package.sh

---
+++ add_package.sh
@@ -0,0 +1,19 @@
+cd friendlywrt-rk3328/friendlywrt
+git clone https://github.com/Lienol/openwrt-package.git
+mv openwrt-package/lienol/luci-app-passwall/ ./package
+git clone https://github.com/songchenwen/nanopi-r2s.git
+mv nanopi-r2s/luci-app-r2sflasher/ ./package
+rm -rf nanopi-r2s/
+git clone https://github.com/NateLol/luci-app-oled.git
+mv luci-app-oled/ ./package
+git clone https://github.com/project-openwrt/luci-app-koolproxyR.git
+mv luci-app-koolproxyR/ ./package
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
+mv luci-app-adguardhome/ ./package
+#git clone https://github.com/garypang13/luci-app-eqos.git
+#mv luci-app-eqos/ ./package
+#git clone https://github.com/vernesong/OpenClash.git
+#mv OpenClash/luci-app-openclash/ ./package
+#rm -rf OpenClash/
+#git clone https://github.com/frainzy1477/luci-app-clash.git
+#mv luci-app-clash/ ./package
